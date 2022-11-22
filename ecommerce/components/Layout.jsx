73e87cb3f0181c60b36f777e2014719d24cbce12
Gabriel Renard <gabriel.renard@protonmail.com>
2022-11-22 22:53:07 +0100

    populate layout component

--- ecommerce/components/Layout.jsx
+++ ecommerce/components/Layout.jsx
@@ -1,7 +1,23 @@
 import React from "react";
+import Head from "next/head"; //same as head in html
+import Navbar from "./Navbar";
+import Footer from "./Footer";
 
 const Layout = () => {
-  return <div>Layout</div>;
+  return (
+    <div className="layout">
+      <Head>
+        <title>Ecommerce store</title>
+      </Head>
+      <header>
+        <Navbar />
+      </header>
+      <main className="main-container">EMPTY</main>
+      <footer>
+        <Footer />
+      </footer>
+    </div>
+  );
 };
 
 export default Layout;
