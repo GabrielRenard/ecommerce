2879676458b50fa8e10e632ad2176e2eee1a0f24
Gabriel Renard <gabriel.renard@protonmail.com>
2022-11-22 15:51:29 +0100

    create HeroBanner component with non dynamic data

--- ecommerce/pages/index.js
+++ ecommerce/pages/index.js
@@ -1,15 +1,16 @@
 import React from "react";
+import { Product, FooterBanner, HeroBanner } from "../components";
 
 const Home = () => {
   return (
     <>
-      HeroBanner
+      <HeroBanner />
       <div>
         <h2>Best Selling Products</h2>
         <p>Speakers of many variations</p>
       </div>
       <div>{["product 1, product 2"].map(product => product)}</div>
-      Footer
+      <FooterBanner />
     </>
   );
 };
