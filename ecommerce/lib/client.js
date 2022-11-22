87c45bea12317321cb6adca8fca06e2f61869fba
Gabriel Renard <gabriel.renard@protonmail.com>
2022-11-22 16:04:18 +0100

    add sanity client in client.js and env folder to store token

---
+++ ecommerce/lib/client.js
@@ -0,0 +1,10 @@
+import sanityClient from "@sanity/client";
+import imageUrlBuilder from "@sanity/image-url";
+
+export const client = sanityClient({
+  projectId: "kmqr1y4o",
+  dataset: "production",
+  apiVersion: "2022-11-22",
+  useCdn: true,
+  token: process.env.NEXT_PUBLIC_SANITY_TOKEN,
+});
